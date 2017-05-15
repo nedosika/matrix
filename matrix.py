f365144f111c13d7da4c735a980d262214bb329a
Pavel <nedosika@mail.ru>
2017-05-15 08:36:36 +0300

    add clear method

--- matrix.py
+++ matrix.py
@@ -1,1 +1,12 @@
 import random, os, sys, termcolor
+
+def clear():
+	if sys.platform=='win32':							#Очиска экрана в зависимости от ОС
+		os.system('cls')
+	else:
+		os.system('clear')
+	
+clear()
+print(termcolor.colored("a", 'red', attrs=['bold']), end = ' ')
+print(termcolor.colored("f", 'red', attrs=['bold']), end = ' ')
+print(termcolor.colored("sd", 'blue', attrs=['bold']), end = ' ')
