1460a91cbde1f87427c6dac40ec81e016c17f7ee
Pavel <nedosika@mail.ru>
2017-05-16 16:05:43 +0300

    updateCurCol

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 import sys, os, random, string, time, shutil, termcolor
 
 def clear():
-	if sys.platform=='win32':			#Очиска экрана в зависимости от ОС
+	if sys.platform=='win32':								#Очиска экрана в зависимости от ОС
 		os.system('cls')
 	else:
 		os.system('clear')
@@ -18,9 +18,9 @@ def render(matrix, rows, cols):
 
 def createRndMatrix(rows, cols):
 	matrix = []
-	for row in range(rows): 					# ROW строк
-		matrix.append([]) 						# создаем пустую строку
-		for col in range(cols): 				# в каждой строке - cols элементов
+	for row in range(rows): 								#ROW строк
+		matrix.append([]) 									#создаем пустую строку
+		for col in range(cols): 							#в каждой строке - cols элементов
 			matrix[row].append("".join(random.choice(string.digits + string.ascii_letters + string.punctuation))) # добавляем очередной элемент в строку	
 	return matrix
 
@@ -37,6 +37,31 @@ def createRndPosLS(rows, cols, lenth, max_speed):
 		current_col[row][2] = random.randint(1, max_speed)	#Максимальная скорость
 	return current_col
 	
+def updateOutMatrix(matrix, rows, cols, current_col):
+	j = 0
+	matrix_out = []
+	for r in range(rows): 									#Перебираем все строки по номерам
+		for c in range(cols): 								#В каждой строке перебираем все столбцы по номерам
+			if (j == current_col[c][0]):
+				matrix_out.append(termcolor.colored(matrix[r][c], 'green', attrs=['bold']))
+			else:
+				if (j < current_col[c][0] and j > current_col[c][0] - current_col[c][1]):
+					matrix_out.append(termcolor.colored(matrix[r][c], 'green'))
+				else:
+					matrix_out.append(" ")
+		j += 1
+	return matrix_out
+	
+def updateCurCol(current_col, rows, cols, lenth, max_speed):
+	for i in range(cols):
+		if (current_col[i][0] - current_col[i][1] > rows):
+			current_col[i][0] = 0
+			current_col[i][1] = random.randint(lenth, rows)
+			current_col[i][2] = random.randint(1, max_speed)
+		else:
+			current_col[i][0] += current_col[i][2]
+	return current_col
+	
 COLS, ROWS = shutil.get_terminal_size()	
 MIN_LENTH_STRING = 10
 MAX_SPEED_SYMBOL = 3
@@ -47,28 +72,9 @@ matrix = createRndMatrix(ROWS, COLS)
 current_col = createRndPosLS(ROWS, COLS, MIN_LENTH_STRING, MAX_SPEED_SYMBOL)
 
 while(1):
-	j = 0
-	matrix_out = []
-	for r in range(ROWS): 									#Перебираем все строки по номерам
-		for c in range(COLS): 								#В каждой строке перебираем все столбцы по номерам
-			if (j == current_col[c][0]):
-				matrix_out.append(termcolor.colored(matrix[r][c], 'green', attrs=['bold']))
-			else:
-				if (j < current_col[c][0] and j > current_col[c][0] - current_col[c][1]):
-					matrix_out.append(termcolor.colored(matrix[r][c], 'green'))
-				else:
-					matrix_out.append(" ")
-		j += 1
-
+	matrix_out = updateOutMatrix(matrix, ROWS, COLS, current_col)
 	render(matrix_out, ROWS, COLS)	
-	
-	for i in range(COLS):
-		if (current_col[i][0] - current_col[i][1] > ROWS):
-			current_col[i][0] = 0
-			current_col[i][1] = random.randint(10, ROWS)
-		else:
-			current_col[i][0] += current_col[i][2]
-	
+	current_col = updateCurCol(current_col, ROWS, COLS, MIN_LENTH_STRING, MAX_SPEED_SYMBOL)
 	time.sleep(DELAY)
 	
 	
