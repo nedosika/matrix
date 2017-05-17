de029e158b7d4cf547699cbb36491cfc28bc6d9d
nedosika <nedosika@mail.ru>
2017-05-17 22:12:39 +0300

    v 1.1a

--- matrix.py
+++ matrix.py
@@ -1,6 +1,12 @@
-﻿#Ver. 1.0
-
+﻿#Ver. 1.1a
 import sys, os, random, string, time, shutil, termcolor
+	
+MIN_LENTH_STRING = 0
+MAX_SPEED_SYMBOL = 3
+DELAY = 0
+
+cols, rows = shutil.get_terminal_size()
+array_current_column = []
 
 def clear():
 	if sys.platform=='win32':								#Очиска экрана в зависимости от ОС
@@ -8,12 +14,11 @@ def clear():
 	else:
 		os.system('clear')
 		
-def render(out_string, rows, cols):
+def render(out_string):
 	sys.stdout.write(out_string)
-	#sys.stdout.flush()
 	sys.stdout.write('\b' * rows * cols)
 
-def createRndMatrix(rows, cols):
+def generateMatrix():
 	matrix = []
 	for row in range(rows): 								#ROW строк
 		matrix.append([]) 									#создаем пустую строку
@@ -21,65 +26,63 @@ def createRndMatrix(rows, cols):
 			matrix[row].append(random.choice(string.digits + string.ascii_letters + string.punctuation)) # добавляем очередной элемент в строку	
 	return matrix
 
-def createRndPosLS(rows, cols, lenth, max_speed):
-	current_col = []
-
+def generateRndPosLS():
+	global array_current_column
+	array_current_column = []
 	for row in range(cols):
-		current_col.append([])
+		array_current_column.append([])
 		for col in range(3):
-			current_col[row].append([])
+			array_current_column[row].append([])
 
-		current_col[row][0] = random.randint(0, rows) 		#Случайное расположение светлого символа
-		current_col[row][1] = random.randint(lenth, rows) 	#Минимальная длина полоски
-		current_col[row][2] = random.randint(1, max_speed)	#Максимальная скорость
-	return current_col
+		array_current_column[row][0] = random.randint(0, rows) 					#Случайное расположение светлого символа
+		array_current_column[row][1] = random.randint(MIN_LENTH_STRING, rows) 	#Минимальная длина полоски
+		array_current_column[row][2] = random.randint(1, MAX_SPEED_SYMBOL)		#Максимальная скорость
 	
-def updateOutMatrix(matrix, rows, cols, current_col):
+def generateOutString(matrix):
 	j = 0
 	out_str = ""
-	for r in range(rows): 									#Перебираем все строки по номерам
-		for c in range(cols): 								#В каждой строке перебираем все столбцы по номерам
-			if (j == current_col[c][0]):
+	
+	for r in range(rows): 														#Перебираем все строки по номерам
+		for c in range(cols): 													#В каждой строке перебираем все столбцы по номерам
+			if (j == array_current_column[c][0]):
 				out_str += termcolor.colored(matrix[r][c], 'green', attrs=['bold'])
-			elif (j < current_col[c][0] and j > current_col[c][0] - current_col[c][1]):
+			elif (j < array_current_column[c][0] and j > array_current_column[c][0] - array_current_column[c][1]):
 				out_str += termcolor.colored(matrix[r][c], 'green')
 			else:
 				out_str += " "
 		j += 1
+		
 	return out_str
 	
-def updateCurCol(current_col, rows, cols, lenth, max_speed):
+def updateArrCurCol():
 	for i in range(cols):
-		if (current_col[i][0] - current_col[i][1] > rows):
-			current_col[i][0] = 0
-			current_col[i][1] = random.randint(lenth, rows)
-			current_col[i][2] = random.randint(1, max_speed)
+		if (array_current_column[i][0] - array_current_column[i][1] > rows):
+			array_current_column[i][0] = 0
+			array_current_column[i][1] = random.randint(MIN_LENTH_STRING, rows)
+			array_current_column[i][2] = random.randint(1, MAX_SPEED_SYMBOL)
 		else:
-			current_col[i][0] += current_col[i][2]
-	return current_col
+			array_current_column[i][0] += array_current_column[i][2]
 
-def loop(matrix, rows, cols, min_lenth_string, max_speed_symbol, current_col):
+def loop(matrix):
+	global rows
+	global cols
 	while(1):
 		new_cols, new_rows = shutil.get_terminal_size()	
 		if (rows == new_rows and cols == new_cols):								#Проверяем не изменился ли размер консоли
-			matrix_out = updateOutMatrix(matrix, rows, cols, current_col)
-			render(matrix_out, rows, cols)	
-			current_col = updateCurCol(current_col, rows, cols, min_lenth_string, max_speed_symbol)
+			render(generateOutString(matrix))	
+			array_current_column = updateArrCurCol()
 			time.sleep(DELAY)
 		else:
 			clear()
 			cols, rows = shutil.get_terminal_size()
-			matrix = createRndMatrix(rows, cols)
-			current_col = createRndPosLS(rows, cols, min_lenth_string, max_speed_symbol)
-	
-COLS, ROWS = shutil.get_terminal_size()	
-MIN_LENTH_STRING = 0
-MAX_SPEED_SYMBOL = 3
-DELAY = 0
+			matrix = generateMatrix()
+			array_current_column = generateRndPosLS()
 
 clear()
-matrix = createRndMatrix(ROWS, COLS)
-current_col = createRndPosLS(ROWS, COLS, MIN_LENTH_STRING, MAX_SPEED_SYMBOL)
-loop(matrix, ROWS, COLS, MIN_LENTH_STRING, MAX_SPEED_SYMBOL, current_col)
+generateRndPosLS()
+loop(generateMatrix())
+
+
+
 
 	
