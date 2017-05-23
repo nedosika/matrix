c2c3a44b441e48b28c159a574a9c46aa696c354d
Pavel <nedosika@mail.ru>
2017-05-23 11:09:14 +0300

    add arrgs version

--- pmatrix.py
+++ pmatrix.py
@@ -1,9 +1,11 @@
 ﻿#Ver. 1.3a
 import sys, os, random, string, time, shutil, termcolor, argparse
+
+version = "1.3a"
 	
 class Property():
 	def __init__( self, rows, cols, min_lenth_string, max_lenth_string, min_speed_symbol, max_speed_symbol ):
-		self.pos_first_symbol = random.randint( -(rows), 0)
+		self.pos_first_symbol = random.randint( -(rows), -3)
 		self.size_string = random.randint( min_lenth_string, max_lenth_string )
 		self.speed = random.randint( min_speed_symbol, max_speed_symbol )
 	
@@ -74,16 +76,20 @@ class PyMatrix():
 			if PyMatrix.checkResize():
 				self.__init__(PyMatrix.min_lenth_string, PyMatrix.max_lenth_string, PyMatrix.min_speed_symbol, PyMatrix.max_speed_symbol, PyMatrix.delay, PyMatrix.color, PyMatrix.attrs)
 			else:
-				PyMatrix.render()	
 				PyMatrix.update()
+				PyMatrix.render()	
 				time.sleep(PyMatrix.delay)
 
 def createParser ():
-	parser = argparse.ArgumentParser()
-	parser.add_argument ('-d', '--delay', type = int, default = 4, choices = [0, 1, 2, 3, 4])
-	parser.add_argument ('-c', '--color', type = str, default = 'green', choices = ['green', 'red', 'blue', 'white', 'yellow'])
-	parser.add_argument ('-u', '--underline', action = 'store_const', const = True, default = False)
-
+	parser = argparse.ArgumentParser(
+		prog = 'pmatrix',
+		description = '''Программа, которая создает эффект матрицы.''',
+		epilog = '''(c) Pavel Nedosika 2017.''')
+	parser.add_argument ('-d', '--delay', type = int, default = 4, choices = [0, 1, 2, 3, 4], help = 'Скорость обновления матрицы')
+	parser.add_argument ('-c', '--color', type = str, default = 'green', choices = ['green', 'red', 'blue', 'white', 'yellow'], help = 'Цвет всей матрицы')
+	parser.add_argument ('-C', type = str, default = 'green', choices = ['green', 'red', 'blue', 'white', 'yellow'], help = 'Цвет первой буквы в строках матрицы')
+	parser.add_argument ('-u', '--underline', action = 'store_const', const = True, default = False, help = 'Подчеркивание первой буквы в строках матрицы')
+	parser.add_argument ('-v', '--version', action='version', help = 'Вывести номер версии', version='%(prog)s {}'.format (version))
 	return parser
 	
 def main():
@@ -93,7 +99,7 @@ def main():
 		attrs = ['bold', 'underline']
 	else:
 		attrs = ['bold']
-	PyMatrix(5, 15, 1, 3, namespace.delay * 0.1, namespace.color, attrs).loop()
+	PyMatrix(5, 20, 1, 3, namespace.delay * 0.1, namespace.color, attrs).loop()
 		
 if __name__ == "__main__":
 	main()
