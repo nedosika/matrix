12f8315e6eaf40357259970370a20d12f3d3791a
Pavel <nedosika@mail.ru>
2017-05-23 11:42:23 +0300

    upd

--- pmatrix.py
+++ pmatrix.py
@@ -4,7 +4,7 @@ import sys, os, random, string, time, shutil, termcolor, argparse
 version = "1.3a"
 	
 class Property():
-	def __init__( self, rows, cols, min_lenth_string, max_lenth_string, min_speed_symbol, max_speed_symbol ):
+	def __init__(self, rows, cols, min_lenth_string, max_lenth_string, min_speed_symbol, max_speed_symbol):
 		self.pos_first_symbol = random.randint( -(rows), -3)
 		self.size_string = random.randint( min_lenth_string, max_lenth_string )
 		self.speed = random.randint( min_speed_symbol, max_speed_symbol )
@@ -17,16 +17,23 @@ class PyMatrix():
 		for row in range(PyMatrix.rows):
 			for col in range(PyMatrix.cols): 													
 				if row == properties[col].pos_first_symbol:
-					sys.stdout.write( termcolor.colored( PyMatrix.matrix[row][col], PyMatrix.color, attrs = PyMatrix.attrs ) )
+					sys.stdout.write( 
+						termcolor.colored(
+							PyMatrix.matrix[row][col], 
+							PyMatrix.color, 
+							attrs = PyMatrix.attrs ) )
 				elif row < properties[col].pos_first_symbol and row > properties[col].pos_first_symbol - properties[col].size_string:
-					sys.stdout.write( termcolor.colored( PyMatrix.matrix[row][col], PyMatrix.color ) )
+					sys.stdout.write(
+						termcolor.colored(
+							PyMatrix.matrix[row][col],
+							PyMatrix.color ) )
 				else:
 					sys.stdout.write( " " )
 		sys.stdout.write('\b' * PyMatrix.rows * PyMatrix.cols)
 	
 	@staticmethod
 	def clear():
-		if sys.platform=='win32':								
+		if sys.platform == 'win32':								
 			os.system('cls')
 		else:
 			os.system('clear')
@@ -44,8 +51,8 @@ class PyMatrix():
 		for property in PyMatrix.properties:
 			if property.pos_first_symbol - property.size_string > PyMatrix.rows:
 				property.pos_first_symbol = 0
-				property.size_string = random.randint( PyMatrix.min_lenth_string, PyMatrix.max_lenth_string )
-				property.speed = random.randint( PyMatrix.min_speed_symbol, PyMatrix.max_speed_symbol )
+				property.size_string = random.randint(PyMatrix.min_lenth_string, PyMatrix.max_lenth_string)
+				property.speed = random.randint(PyMatrix.min_speed_symbol, PyMatrix.max_speed_symbol)
 			else:
 				property.pos_first_symbol += property.speed		
 			
@@ -66,15 +73,27 @@ class PyMatrix():
 				PyMatrix.matrix[row].append(random.choice(string.digits + string.ascii_letters + string.punctuation))
 
 		PyMatrix.properties = []
-		for i in range( PyMatrix.cols ):
-			PyMatrix.properties.append( Property( PyMatrix.rows, PyMatrix.cols, min_lenth_string, max_lenth_string, min_speed_symbol, max_speed_symbol ) )
-			
+		for i in range(PyMatrix.cols):
+			PyMatrix.properties.append( Property( 
+											PyMatrix.rows, 
+											PyMatrix.cols, 
+											min_lenth_string, 
+											max_lenth_string, 
+											min_speed_symbol, 
+											max_speed_symbol ) )
 		PyMatrix.clear()
 	
 	def loop(self):
 		while(True):
 			if PyMatrix.checkResize():
-				self.__init__(PyMatrix.min_lenth_string, PyMatrix.max_lenth_string, PyMatrix.min_speed_symbol, PyMatrix.max_speed_symbol, PyMatrix.delay, PyMatrix.color, PyMatrix.attrs)
+				self.__init__(
+					PyMatrix.min_lenth_string, 
+					PyMatrix.max_lenth_string, 
+					PyMatrix.min_speed_symbol, 
+					PyMatrix.max_speed_symbol, 
+					PyMatrix.delay, 
+					PyMatrix.color, 
+					PyMatrix.attrs)
 			else:
 				PyMatrix.update()
 				PyMatrix.render()	
@@ -84,38 +103,45 @@ def createParser ():
 	parser = argparse.ArgumentParser(
 		prog = 'pmatrix',
 		description = '''Программа, которая создает эффект матрицы.''',
-		epilog = '''(c) Pavel Nedosika 2017.''')
-		
+		epilog = '''(c) Pavel Nedosika 2017.''',
+		add_help = False,
+		)
+	
 	parser.add_argument (
-		'-d', '--delay', 
+		'--help', '-h', 
+		action = 'help', 
+		help = 'Справка')
+	
+	parser.add_argument (
+		'--delay', '-d', 
 		type = int, 
 		default = 4, 
 		choices = [0, 1, 2, 3, 4], 
 		help = 'Скорость обновления матрицы')
 		
 	parser.add_argument (
-		'-c', '--color', 
+		'--color', '-c', 
 		type = str, 
 		default = 'green', 
 		choices = ['green', 'red', 'blue', 'white', 'yellow'], 
 		help = 'Цвет всей матрицы')
 		
 	parser.add_argument (
-		'-C', 
+		'--first_symbol', '-C',
 		type = str, 
 		default = 'green', 
 		choices = ['green', 'red', 'blue', 'white', 'yellow'], 
 		help = 'Цвет первой буквы в строках матрицы')
 		
 	parser.add_argument (
-		'-u', '--underline', 
+		'--underline', '-u', 
 		action = 'store_const', 
 		const = True, 
 		default = False, 
 		help = 'Подчеркивание первой буквы в строках матрицы')
 		
 	parser.add_argument (
-		'-v', '--version', 
+		'--version', '-v', 
 		action='version', 
 		help = 'Вывести номер версии', 
 		version='%(prog)s {}'.format (version))
