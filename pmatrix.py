2c441694ebc71c0f62dd41d0591168c29cad7026
nedosika <nedosika@mail.ru>
2017-05-23 22:14:16 +0300

    fix color first symbol

--- pmatrix.py
+++ pmatrix.py
@@ -1,4 +1,5 @@
-﻿#Ver. 1.3a
+﻿#!/usr/bin/python3
+#Ver. 1.3a
 import sys, os, random, string, time, shutil, termcolor, argparse
 
 version = "1.3a"
@@ -10,58 +11,6 @@ class Property():
 		self.speed = random.randint( min_speed_symbol, max_speed_symbol )
 	
 class PyMatrix():
-	def render(self):
-		properties = self.properties
-		for row in range(self.rows):
-			for col in range(self.cols): 													
-				if row == properties[col].pos_first_symbol:
-					sys.stdout.write( 
-						termcolor.colored(
-							self.matrix[row][col], 
-							self.first_symbol, 
-							attrs = self.attrs ) )
-				elif row < properties[col].pos_first_symbol and row > properties[col].pos_first_symbol - properties[col].size_string:
-					sys.stdout.write(
-						termcolor.colored(
-							self.matrix[row][col],
-							self.color ) )
-				else:
-					sys.stdout.write( " " )
-		sys.stdout.write('\b' * self.rows * self.cols)
-
-	def clear(self):
-		if sys.platform == 'win32':								
-			os.system('cls')
-		else:
-			os.system('clear')
-
-	def checkResize(self):
-		new_cols, new_rows = shutil.get_terminal_size()	
-		if (self.rows == new_rows and self.cols == new_cols):
-			return False
-		else:
-			return True
-	
-	def update(self):
-		if self.checkResize():
-			self.__init__(
-				self.min_lenth_string, 
-				self.max_lenth_string, 
-				self.min_speed_symbol, 
-				self.max_speed_symbol, 
-				self.delay, 
-				self.color,
-				self.first_symbol,
-				self.attrs)
-		else:
-			for property in self.properties:
-				if property.pos_first_symbol - property.size_string > self.rows:
-					property.pos_first_symbol = 0
-					property.size_string = random.randint(self.min_lenth_string, self.max_lenth_string)
-					property.speed = random.randint(self.min_speed_symbol, self.max_speed_symbol)
-				else:
-					property.pos_first_symbol += property.speed		
-			
 	def __init__(self, min_lenth_string, max_lenth_string, min_speed_symbol, max_speed_symbol, delay, color, first_symbol, attrs):
 		self.cols, self.rows = shutil.get_terminal_size()
 		self.min_lenth_string = min_lenth_string
@@ -81,63 +30,114 @@ class PyMatrix():
 
 		self.properties = []
 		for i in range(self.cols):
-			self.properties.append( Property( 
+			self.properties.append(Property( 
 											self.rows, 
 											self.cols, 
 											self.min_lenth_string, 
 											self.max_lenth_string, 
 											self.min_speed_symbol, 
-											self.max_speed_symbol ) )
+											self.max_speed_symbol))
 		self.clear()
-	
+
+	def render(self):
+		properties = self.properties
+		for row in range(self.rows):
+			for col in range(self.cols): 													
+				if row == properties[col].pos_first_symbol:
+					sys.stdout.write(
+						termcolor.colored(
+							self.matrix[row][col], 
+							self.first_symbol, 
+							attrs = self.attrs))
+				elif row < properties[col].pos_first_symbol and row > properties[col].pos_first_symbol - properties[col].size_string:
+					sys.stdout.write(
+						termcolor.colored(
+							self.matrix[row][col],
+							self.color))
+				else:
+					sys.stdout.write(" ")
+		sys.stdout.write('\b' * self.rows * self.cols)
+
+	def update(self):
+		if self.checkResize():
+			self.__init__(
+				self.min_lenth_string, 
+				self.max_lenth_string, 
+				self.min_speed_symbol, 
+				self.max_speed_symbol, 
+				self.delay, 
+				self.color,
+				self.first_symbol,
+				self.attrs)
+		else:
+			for property in self.properties:
+				if property.pos_first_symbol - property.size_string > self.rows:
+					property.pos_first_symbol = 0
+					property.size_string = random.randint(self.min_lenth_string, self.max_lenth_string)
+					property.speed = random.randint(self.min_speed_symbol, self.max_speed_symbol)
+				else:
+					property.pos_first_symbol += property.speed		
+
 	def loop(self):
 		while(True):
 			self.update()
 			self.render()	
 			time.sleep(self.delay)
+			
+	def clear(self):
+		if sys.platform == 'win32':								
+			os.system('cls')
+		else:
+			os.system('clear')
+
+	def checkResize(self):
+		new_cols, new_rows = shutil.get_terminal_size()	
+		if (self.rows == new_rows and self.cols == new_cols):
+			return False
+		else:
+			return True
 
 def createParser ():
 	parser = argparse.ArgumentParser(
 		prog = 'pmatrix',
 		description = '''Программа, которая создает эффект матрицы.''',
 		epilog = '''(c) Pavel Nedosika 2017.''',
-		add_help = False,
-		)
+		add_help = False)
 	
-	parser.add_argument (
+	parser.add_argument(
 		'--help', '-h', 
 		action = 'help', 
 		help = 'Справка')
 	
-	parser.add_argument (
+	parser.add_argument(
 		'--delay', '-d', 
 		type = int, 
 		default = 4, 
 		choices = [0, 1, 2, 3, 4], 
 		help = 'Скорость обновления матрицы')
 		
-	parser.add_argument (
+	parser.add_argument(
 		'--color', '-c', 
 		type = str, 
 		default = 'green', 
 		choices = ['green', 'red', 'blue', 'white', 'yellow'], 
 		help = 'Цвет всей матрицы')
 		
-	parser.add_argument (
+	parser.add_argument(
 		'--first_symbol', '-C',
 		type = str, 
 		default = 'green', 
 		choices = ['green', 'red', 'blue', 'white', 'yellow'], 
 		help = 'Цвет первой буквы в строках матрицы')
 		
-	parser.add_argument (
+	parser.add_argument(
 		'--underline', '-u', 
 		action = 'store_const', 
 		const = True, 
 		default = False, 
 		help = 'Подчеркивание первой буквы в строках матрицы')
 		
-	parser.add_argument (
+	parser.add_argument(
 		'--version', '-v', 
 		action='version', 
 		help = 'Вывести номер версии', 
